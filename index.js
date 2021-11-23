21a81ab6716f3d911c5e35e162e30b690354b653
safwantaliparamba <safwansafu242090@gmail.com>
2021-11-23 09:03:35 +0530

    admin panel added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const configuration = require('./db/config')
 const Product = require('./models/product')
+const User = require('./models/user')
 const path = require('path')
 const engine = require('ejs-mate')
 const methodOverride = require('method-override')
@@ -13,6 +14,7 @@ app.set('view engine' , 'ejs')
 app.set('View' , path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname ,'assets')))
 
 
 app.get('/', async(req, res) => {
@@ -24,11 +26,13 @@ app.get('/admin', async(req, res) => {
     const allProducts =await Product.find({})
     res.render('admin/index' , {allProducts})
 })
+
 app.get('/admin/products/:id/edit' , async(req, res) => {
     const { id} = req.params
     const product = await Product.findById(id)
     res.render('admin/edit', {product})
 })
+
 app.get('/admin/products/new' , (req, res) => {
     res.render('admin/addpage')
 })
@@ -55,7 +59,14 @@ app.get('/product/:id', async(req, res) => {
     const product = await Product.findById(id)
     res.render('user/prodetails' , {product})
 })
-
+app.get('/addtocart/:id' , async(req, res)=>{
+    const { id} = req.params
+    const newuser = await User.findById('61963f90bba23163e098e6ad')
+    newuser.cart.push(id)
+    await newuser.save()
+    console.log(newuser);
+    res.send('success')
+})
 
 app.listen(3000 , ()=> {
     console.log('shopping app listening on port 3000')
